50ad914a70059c1ddb18301ed808eaa3332ea299
manish-mehra <manishmehra1469@gmail.com>
2022-03-05 12:28:25 +0530

    fix: spelling error

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ export default function Hero() {
         <div className="hero--heading">
             <p className='hero--title'>
                 <b>Hi, I'm Manish Mehra.</b> <br/>
-                I'm a student at Indraprasath University <br/>
+                I'm a student at Indraprastha University <br/>
                 New Delhi, pursuing BCA<sup style={{fontSize: "14px"}}>(bachelor's in computer application)</sup> <br/>
                 and an aspiring <b>web developer</b>.
             </p>
@@ -14,7 +14,7 @@ export default function Hero() {
             <p className='hero--subtitle'>
             I'm passionate about creating web apps and I want to use  <br/> 
             technology to come up with creative solutions. You can find me on<br/> 
-             <a href="https://github.com/manish-mehra">github</a>, or <a href = "mailto: itsmanishmehra@gmail.com">send an email</a>.
+             <a href="https://github.com/manish-mehra" target="_blank">github</a>, or <a href = "mailto: itsmanishmehra@gmail.com">send an email</a>.
             </p>
         </div>
     </div>
